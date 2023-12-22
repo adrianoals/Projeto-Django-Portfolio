e3fbecb9d3b11d7e94a200caf089cbf1beb4fd51
Adriano Lima <dri.limasantos@gmail.com>
2023-12-22 16:03:13 -0300

    Up

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ProjetoWebAPI(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    link = models.URLField(max_length=200)
+    imagem = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from portfolio.models import ProjetoWebAPI
 
 def index(request):
         return render(request, 'portfolio/index.html')
 
 def projeto(request):
-        return render(request, 'portfolio/projeto.html')
+        projetos = ProjetoWebAPI.objects.all()
+        return render(request, 'portfolio/projeto.html', {"cards": projetos})
 
 def contato(request):
         return render(request, 'portfolio/contato.html')
