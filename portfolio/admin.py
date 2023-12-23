c849e65293bc29179ba65ff2bd4e4409e8606870
Adriano Lima <dri.limasantos@gmail.com>
2023-12-23 01:20:44 -0300

    up

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from portfolio.models import ProjetoWebAPI
 
-# Register your models here.
+class ProjetoWebApiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'link')
+    list_display_links = ('id', 'nome', 'link')
+
+
+admin.site.register(ProjetoWebAPI, ProjetoWebApiAdmin)
