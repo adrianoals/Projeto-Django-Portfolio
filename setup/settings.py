a243caecd22140eb6fe258b8c6cb9893f00fbd65
Adriano Lima <dri.limasantos@gmail.com>
2023-12-24 18:02:49 -0300

    up

--- setup/settings.py
+++ setup/settings.py
@@ -139,7 +139,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'adrianotesteapp@gmail.com'  # Substitua pelo seu e-mail do Gmail
+EMAIL_HOST_USER = 'adrianotesteapp@gmail.com'  
 EMAIL_HOST_PASSWORD = (os.getenv('EMAIL_HOST_PASSWORD'))
-
-# EMAIL_HOST_PASSWORD = 'zmrq cgmd jqmr oilv'  # Substitua pela senha do seu e-mail do Gmail
