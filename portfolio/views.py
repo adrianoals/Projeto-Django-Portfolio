1bff783a80c5dfbc9725d8b2a96fb3f6840859e6
Adriano Lima <dri.limasantos@gmail.com>
2023-12-24 10:47:02 -0300

    up

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,7 +7,7 @@ def index(request):
 def projeto(request):
         projetos_web_api = ProjetoWebAPI.objects.all()
         projetos_automacoes = ProjetoAutomacao.objects.all()
-        return render(request, 'portfolio/projeto.html', {"projetos_web_api": projetos_web_api}, {"projetos_automacoes" : projetos_automacoes })
+        return render(request, 'portfolio/projeto.html', {"projetos_web_api": projetos_web_api, "projetos_automacoes" : projetos_automacoes,})
 
 def contato(request):
         return render(request, 'portfolio/contato.html')
