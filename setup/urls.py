0915e4654f3349dcdca0794e59986e8a7cbe70aa
Adriano Lima <dri.limasantos@gmail.com>
2023-12-26 15:49:51 -0300

    UP

--- setup/urls.py
+++ setup/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
@@ -25,3 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
