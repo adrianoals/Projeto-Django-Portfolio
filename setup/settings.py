1cf89c7d26575b18dbdef3851624bce814010e50
Adriano Lima <dri.limasantos@gmail.com>
2024-01-28 21:17:25 -0300

    up

--- setup/settings.py
+++ setup/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # SQL LITE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-# Postgrees
+# # Postgrees SUPABASE
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -100,6 +100,18 @@ DATABASES = {
 #         'PORT': '5432'
 #     }
 # }
+
+# Postgrees Railway
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway', 
+        'USER': 'postgres', 
+        'PASSWORD': '35dA5gFFGCeEEEb*G35*3A66dEDfFE35' ,
+        'HOST': 'viaduct.proxy.rlwy.net', 
+        'PORT': '37529'
+    }
+}
 
 
 # Password validation
