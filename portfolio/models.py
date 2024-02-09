70a3474d2306305a50c83dd5fa7e9cba1340980e
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2024-02-09 19:19:59 -0300

    alteracoes de midia

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,8 @@ from datetime import datetime
 class ProjetoWebAPI(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     link = models.URLField(max_length=300)
-    imagem = models.CharField(max_length=150, null=False, blank=False)
+    # imagem = models.CharField(max_length=150, null=False, blank=False)
+    imagem = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     data_criacao = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
